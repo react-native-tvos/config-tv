95bcaea08c86c5ce37d918dbd4f2ff1c9fe0f09a
Douglas Lowder <douglowder@mac.com>
2023-10-27 12:46:27 -0700

    Make tvOS deployment target configurable

--- packages/config-tv/src/withTVXcodeProject.ts
+++ packages/config-tv/src/withTVXcodeProject.ts
@@ -1,39 +1,51 @@
-import { ExpoConfig } from "@expo/config-types";
+import { ExpoConfig } from '@expo/config-types';
 import {
   ConfigPlugin,
   WarningAggregator,
   withXcodeProject,
   XcodeProject,
-} from "expo/config-plugins";
+} from 'expo/config-plugins';
 
-import { ConfigData } from "./types";
-import { isTVEnabled, showVerboseWarnings } from "./utils";
+import { ConfigData } from './types';
+import {
+  isTVEnabled,
+  showVerboseWarnings,
+  tvosDeploymentTarget,
+} from './utils';
 
-const pkg = require("../package.json");
+const pkg = require('../package.json');
 
 export const withTVXcodeProject: ConfigPlugin<ConfigData> = (
   config,
-  params
+  params,
 ) => {
   const isTV = isTVEnabled(params);
   const verbose = showVerboseWarnings(params);
+  const deploymentTarget = tvosDeploymentTarget(params);
   return withXcodeProject(config, async (config) => {
     config.modResults = await setXcodeProjectBuildSettings(config, {
       project: config.modResults,
       isTV,
       verbose,
+      deploymentTarget,
     });
     return config;
   });
 };
 
 export function setXcodeProjectBuildSettings(
-  config: Pick<ExpoConfig, "ios">,
+  config: Pick<ExpoConfig, 'ios'>,
   {
     project,
     isTV,
     verbose,
-  }: { project: XcodeProject; isTV: boolean; verbose: boolean }
+    deploymentTarget,
+  }: {
+    project: XcodeProject;
+    isTV: boolean;
+    verbose: boolean;
+    deploymentTarget: string;
+  },
 ): XcodeProject {
   const deviceFamilies = formatDeviceFamilies(getDeviceFamilies(config));
   const configurations = project.pbxXCBuildConfigurationSection();
@@ -41,33 +53,33 @@ export function setXcodeProjectBuildSettings(
   for (const { buildSettings } of Object.values(configurations || {})) {
     // Guessing that this is the best way to emulate Xcode.
     // Using `project.addToBuildSettings` modifies too many targets.
-    if (typeof buildSettings?.PRODUCT_NAME !== "undefined") {
-      if (isTV && buildSettings.TARGETED_DEVICE_FAMILY !== "3") {
+    if (typeof buildSettings?.PRODUCT_NAME !== 'undefined') {
+      if (isTV && buildSettings.TARGETED_DEVICE_FAMILY !== '3') {
         if (verbose) {
           WarningAggregator.addWarningIOS(
-            "xcodeproject",
+            'xcodeproject',
             `${pkg.name}@${pkg.version}: modifying target ${
               buildSettings?.PRODUCT_NAME
-            } for ${isTV ? "tvOS" : "iOS"}`
+            } for ${isTV ? 'tvOS' : 'iOS'}`,
           );
         }
-        buildSettings.TARGETED_DEVICE_FAMILY = "3";
-        buildSettings.TVOS_DEPLOYMENT_TARGET = "13.0";
-        buildSettings.SDKROOT = "appletvos";
-        if (typeof buildSettings?.IOS_DEPLOYMENT_TARGET !== "undefined") {
+        buildSettings.TARGETED_DEVICE_FAMILY = '3';
+        buildSettings.TVOS_DEPLOYMENT_TARGET = deploymentTarget;
+        buildSettings.SDKROOT = 'appletvos';
+        if (typeof buildSettings?.IOS_DEPLOYMENT_TARGET !== 'undefined') {
           delete buildSettings?.IOS_DEPLOYMENT_TARGET;
         }
-      } else if (!isTV && buildSettings.TARGETED_DEVICE_FAMILY === "3") {
+      } else if (!isTV && buildSettings.TARGETED_DEVICE_FAMILY === '3') {
         WarningAggregator.addWarningIOS(
-          "xcodeproject",
+          'xcodeproject',
           `${pkg.name}@${pkg.version}: modifying target ${
             buildSettings?.PRODUCT_NAME
-          } for ${isTV ? "tvOS" : "iOS"}`
+          } for ${isTV ? 'tvOS' : 'iOS'}`,
         );
         buildSettings.TARGETED_DEVICE_FAMILY = deviceFamilies;
-        buildSettings.IOS_DEPLOYMENT_TARGET = "13.0";
-        buildSettings.SDKROOT = "iphoneos";
-        if (typeof buildSettings?.TVOS_DEPLOYMENT_TARGET !== "undefined") {
+        buildSettings.IOS_DEPLOYMENT_TARGET = deploymentTarget;
+        buildSettings.SDKROOT = 'iphoneos';
+        if (typeof buildSettings?.TVOS_DEPLOYMENT_TARGET !== 'undefined') {
           delete buildSettings?.TVOS_DEPLOYMENT_TARGET;
         }
       }
@@ -83,18 +95,18 @@ export function setXcodeProjectBuildSettings(
  * @param deviceFamilies
  */
 export function formatDeviceFamilies(deviceFamilies: number[]): string {
-  return `"${deviceFamilies.join(",")}"`;
+  return `"${deviceFamilies.join(',')}"`;
 }
 
-export function getSupportsTablet(config: Pick<ExpoConfig, "ios">): boolean {
+export function getSupportsTablet(config: Pick<ExpoConfig, 'ios'>): boolean {
   return !!config.ios?.supportsTablet;
 }
 
-export function getIsTabletOnly(config: Pick<ExpoConfig, "ios">): boolean {
+export function getIsTabletOnly(config: Pick<ExpoConfig, 'ios'>): boolean {
   return !!config?.ios?.isTabletOnly;
 }
 
-export function getDeviceFamilies(config: Pick<ExpoConfig, "ios">): number[] {
+export function getDeviceFamilies(config: Pick<ExpoConfig, 'ios'>): number[] {
   const supportsTablet = getSupportsTablet(config);
   const isTabletOnly = getIsTabletOnly(config);
 
