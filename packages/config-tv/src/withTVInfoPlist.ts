63071c52ba8816001a8695b607f435efc918617b
Douglas Lowder <douglowder@mac.com>
2024-03-12 18:09:59 -0700

    Fix UIRequiredDeviceCapabilities for Apple TV

---
+++ packages/config-tv/src/withTVInfoPlist.ts
@@ -0,0 +1,16 @@
+import { ConfigPlugin, withInfoPlist } from 'expo/config-plugins';
+
+import { ConfigData } from './types';
+import { verboseLog } from './utils';
+
+export const withTVInfoPlist: ConfigPlugin<ConfigData> = (c, params = {}) => {
+  verboseLog('Modifying UIRequiredDeviceCapabilities for TV', {
+    params,
+    platform: 'ios',
+    property: 'Info.plist',
+  });
+  return withInfoPlist(c, (config) => {
+    config.modResults.UIRequiredDeviceCapabilities = ['arm64'];
+    return config;
+  });
+};
